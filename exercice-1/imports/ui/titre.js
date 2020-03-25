adf3bf2b463317253310e223c40c5949332ed39e
Loris Rimaz <loris.rimaz@gmail.com>
2020-03-25 08:17:38 +0100

    Commentaires

--- exercice-1/imports/ui/titre.js
+++ exercice-1/imports/ui/titre.js
@@ -4,23 +4,27 @@ import { ReactiveVar } from 'meteor/reactive-var'
 import './titre.html'
 
 Template.titre.onCreated(function(){
-  // Here, this equals the current template instance. We can assign
-  // our ReactiveVar to it, making it accessible throughout the
-  // current template instance.
+  // Création de la variable réactive
   this.titre = new ReactiveVar( "Titre" );
 });
 
 Template.titre.helpers({
+  // Helper du titre
   titre: function() {
+    // Récupérer la variable réactive
     return Template.instance().titre.get()
   }
 });
 
 Template.titre.events({
+  // Événement sur le formulaire
   "submit form": function(event) {
+    // Empechez le comportement par défaut (rercharger la page)
     event.preventDefault();
+    // Récuperer la cible de l'événement
     let cible = event.target;
     let texte = cible.nouveau_titre.value;
+    // Updater la variable réactive
     Template.instance().titre.set(texte);
   }
 });
